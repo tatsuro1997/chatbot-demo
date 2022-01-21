bde741d45fd2179960da4c6dc6dedf854eee34a9
tatsuro1997 <minagawa@dptr.jp>
2022-01-22 05:49:57 +0900

    最下部にスクロール

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -4,7 +4,7 @@ import { Chat } from './index';
 const Chats = (props) => {
 
   return (
-    <List sx={{height: 400, padding: '0', overflow: 'auto'}}>
+    <List sx={{height: 400, padding: '0', overflow: 'auto'}}　id={"scroll-area"}>
       {props.chats.map((chat, index) => {
         return <Chat text={chat.text} type={chat.type} key={index.toString()} />
       })}
